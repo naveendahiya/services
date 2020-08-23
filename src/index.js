e099c54edea7077cdfc7d5c5fc5bf2fd46bbe813
GAUTAMSAHARAN <gsooouuu@gmail.com>
2020-08-23 17:11:34 +0530

    restart

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/App';
-import './css/global.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
